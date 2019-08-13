d225995d5da26420270126ba70a2e332ebd87ba7
Dominic Tremblay <dominictremblay24@gmail.com>
2019-08-13 11:21:38 -0400

    fixing the test

--- cypress/integration/new_tweet.spec.js
+++ cypress/integration/new_tweet.spec.js
@@ -1,7 +1,7 @@
 describe('New Tweet', () => {
 
 
-  it.only('receives focus when the page loads', () =>{
+  it('receives focus when the page loads', () =>{
 
     cy.visit('/');
 
@@ -9,20 +9,37 @@ describe('New Tweet', () => {
 
     })
 
-  it('submits a new todo', () => {
-
+  it('accepts a user input for a new todo' ,()=> {
     cy.get('#todo-input')
       .type("Get a coffee")
       .should('have.value', "Get a coffee")
-      
+  })
+
+  it('submits a new todo', () => {
+
+    // stub an axio post request to /api/todos
+    
+
     cy.get('#todo-input')  
       .type('{enter}')
 
+      // add reset here
 
     cy.get('.list-group li')
     .should('have.length','2')
 
 })
+
+// should not submit empty todo
+
+// delete a todo
+// click ont the first button
+// check that label contains 'wal the dog is not visible'
+
+//Check a todo
+// click the first check
+// check that the first check is checked
+
 
 
 })
